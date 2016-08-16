7e5d9639141d8503abb7c5d69a361f9b53db841c
Zino <rhu5@u.rochester.edu>
2016-08-16 16:18:00 -0500

    modify admin and item models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,5 @@ class Admin < ActiveRecord::Base
 	has_secure_password
 
 	validates :username, presence: true	
+	validates :password, presence: true, allow_nil: true
 end
---
+++ app/models/item.rb
@@ -0,0 +1,5 @@
+class Item < ActiveRecord::Base
+	
+	validates :name, presence: true
+	validates :price, presence: true
+end
