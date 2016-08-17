53d4dd660adabb221f040dda52e12628f8ccdeb4
Zino <rhu5@u.rochester.edu>
2016-08-17 16:21:52 -0500

    remove stub and refactor login and logout integration tests
    Add log_in_as helper method to extract login logic for integration
    tests.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,17 +10,10 @@ RSpec.describe "Log In" do
 		end
 
 		it "redirects and flashes when valid" do
-			visit login_path
-			fill_in "session[username]", with: "zino"
-			fill_in "session[password]", with: "dasda"
+			Admin.create(username: "zino", password: "dasda", password_confirmation: "dasda")
+		
+			log_in_as("zino", "dasda")
 
-			admin_stub = instance_double(Admin)
-			expect(admin_stub).to receive(:authenticate).and_return(true)
-			expect(admin_stub).to receive(:id).and_return(123)
-			expect(admin_stub).to receive(:username).at_least(:twice).and_return("zino")
-			expect(Admin).to receive(:find_by).at_least(:twice).and_return(admin_stub)
-
-			click_on("commit")
 			current_path.should eq(items_path)
 			expect(page).to have_selector(".ui.success.message", count: 1)
 			expect(page).to have_selector("#username", count: 1)
--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -2,20 +2,11 @@ RSpec.describe "Log Out" do
 
 	describe "delete session" do
 		it "redirects and flashes" do
-			#login TODO: remove this section
-			visit login_path
-			fill_in "session[username]", with: "zino"
-			fill_in "session[password]", with: "dasda"
+			Admin.create(username: "zino", password: "dasda", password_confirmation: "dasda")
+			
+			log_in_as("zino","dasda")
 
-			admin_stub = instance_double(Admin)
-			expect(admin_stub).to receive(:authenticate).and_return(true)
-			expect(admin_stub).to receive(:id).and_return(123)
-			expect(admin_stub).to receive(:username).at_least(:twice).and_return("zino")
-			expect(Admin).to receive(:find_by).at_least(:twice).and_return(admin_stub)
-
-			click_on("commit")
-
-			#logout
+			visit root_url
 			click_on("Log Out")
 			expect(page).to have_selector(".ui.success.message", count: 1)
 			expect(page).to have_selector("#username", count: 0)
