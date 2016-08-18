79843a5a6c8798b66d33bfc776e82f3de538fa03
Zino <rhu5@u.rochester.edu>
2016-08-18 10:04:28 -0500

    implement selection model & controller
    implement selection model; add association with item model; add
    selections controller

---
+++ app/controllers/selections_controller.rb
@@ -0,0 +1,31 @@
+class SelectionsController < ApplicationController
+	before_action :valid_item, only: [:create]
+	before_action :valid_selection, only: [:destroy]
+
+	def create
+		session[:selection_owner_hash] ||= Selection.generate_owner_hash
+		Selection.find_or_create_by(owner_hash: session[:selection_owner_hash], item_id: @item.id)
+		redirect_and_flash(root_path, :success, "Item added.")
+	rescue
+		redirect_and_flash(root_path, :error, "Error!")
+	end
+
+	def destroy
+		@selection.destroy
+		redirect_and_flash(root_path, :success, "Selection removed!")
+	end	
+
+	private
+
+		def valid_item
+			@item = Item.find(params[:item_id])
+		rescue ActiveRecord::RecordNotFound
+			redirect_and_flash[root_path, :error, "Invalid item!"]
+		end
+
+		def valid_selection
+			@selection = Selection.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			redirect_and_flash[root_path, :error, "Invalid selection!"]
+		end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
-	
 	validates :name, presence: true
 	validates :price, presence: true
+
+	has_many :selections
 end
