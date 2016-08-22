a85b133771a54bf4c5c9ef4de09e729e7b7f6903
Zino <rhu5@u.rochester.edu>
2016-08-22 15:57:35 -0500

    #17 add access control for item controller#create and #new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-
+  before_action :valid_admin, only: [:new, :create]
+  
   def index
     @selections = Selection.where(owner_hash: session[:selection_owner_hash])
     @categories = Item.group_by_category(Item.all.to_a - @selections.to_a.map(&:item))
@@ -22,6 +23,10 @@ class ItemsController < ApplicationController
   def item_create_params
     params[:item][:special_date] = params[:item][:special_date].to_i if params[:item][:special_date].present?
     params.require(:item).permit(:name, :description, :category, :price, :special_date)
+  end
+
+  def valid_admin
+    redirect_and_flash(login_path, :error, "Please log in first") unless logged_in?
   end
 
 end
