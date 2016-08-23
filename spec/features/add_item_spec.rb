478547608cdb5dbbdd5e23cc968f00b57526631f
Zino <rhu5@u.rochester.edu>
2016-08-23 16:49:03 -0500

    fix syntax errors;give model validation errors better html class; modify add_item_spec to use the new error class

--- spec/features/add_item_spec.rb
+++ spec/features/add_item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "New Item" do
       fill_in "item[price]", with: ""
       click_on "Create Item"
 
-      expect(page).to have_selector(".list li", count: 2)
+      expect(page).to have_selector("li.validation-error", count: 2)
     end
 
     it "redirects and flashes when valid" do
@@ -20,8 +20,8 @@ RSpec.describe "New Item" do
       fill_in "item[price]", with: 1.23
       click_on "Create Item"
 
-  		expect(current_path).to eq(items_path)
-  		expect(page).to have_selector(".ui.success.message", count: 1)
+      expect(current_path).to eq(items_path)
+      expect(page).to have_selector(".ui.success.message", count: 1)
   	end
 
   	it "should be default to not have a special date" do
@@ -30,7 +30,7 @@ RSpec.describe "New Item" do
   		fill_in "item[price]", with: 1.24
   		click_on "Create Item" 
 
-  		expect(page).to have_selector(".meta", :text => /^\$ 1.24$/, count: 1)
+  		expect(page).to have_selector(".meta", text: /^\$ 1.24$/, count: 1)
   	end	
   end
 end
