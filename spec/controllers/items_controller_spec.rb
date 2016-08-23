d8c5ea46b1cd646b073c30046622dde07715cd17
Zino <rhu5@u.rochester.edu>
2016-08-23 09:53:48 -0500

    fix syntax issues

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ItemsController, type: :controller do
 
     it "get form for new item when logged in" do
       session[:admin_id] = "123"
-      allow(Admin).to receive(:find_by).with({ :id => "123" }).and_return(true)
+      allow(Admin).to receive(:find_by).with( { id: "123" } ).and_return(true)
 
       get :new
       expect(response).to render_template(:new)
@@ -19,24 +19,24 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "POST create" do
     it "redirects when not logged in" do
-      post :create, item:{ name: "item1", price: 1.2 }
+      post :create, item: { name: "item1", price: 1.2 }
       expect(response).to redirect_to(login_path)
     end
 
     it "creates new item when valid" do
       session[:admin_id] = "123"
-      allow(Admin).to receive(:find_by).with({ :id => "123" }).and_return(true)
+      allow(Admin).to receive(:find_by).with( { id: "123" } ).and_return(true)
 
-      post :create, item:{ name: "item1", price: 1.2 }
+      post :create, item: { name: "item1", price: 1.2 }
       expect(response).to redirect_to(items_path)
       expect(assigns(:item).name).to eq("item1")
     end
 
     it "returns to new item form when invalid" do
       session[:admin_id] = "123"
-      allow(Admin).to receive(:find_by).with({ :id => "123" }).and_return(true)
+      allow(Admin).to receive(:find_by).with( { id: "123" } ).and_return(true)
 
-      post :create, item:{ name: "", price: "" }
+      post :create, item: { name: "", price: "" }
       expect(response).to render_template(:new)
       expect(assigns(:item)).to be_present
     end
